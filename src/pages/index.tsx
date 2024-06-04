5144836f345a4b3dadccb8784e6bbf91e1a73087
Sudip Kundu <sudipkundu999@gmail.com>
2024-06-05 00:21:32 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,6 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import NotesComponent from "./components/NotesComponent";
 
-
 const Home: NextPage = () => {
   const { data: sessionData, status } = useSession();
   return (
